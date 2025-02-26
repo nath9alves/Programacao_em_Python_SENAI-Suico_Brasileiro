26aeed044ed513ecf9035210773d59e0654aa509
Nathalia <156125029+nath9alves@users.noreply.github.com>
2025-02-25 22:18:31 -0300

    Create Um Outro Uso do Print.py

---
+++ Um Outro Uso do Print.py
@@ -0,0 +1,8 @@
+"""# Um Outro Uso do Print"""
+
+print('Meu nome é', 'Python', 'Ok?')    # Ele exibe as três strings de forma unida
+
+print('Meu nome é', 'Python', 'Ok?', sep=' $$ ')     # com sep='texto' ao final do código é realizada a separação
+
+print('Meu nome é Python', 'Ok?')
+print('Monty Python', end='   ....!')     # o comando end='texto' insere um texto ao final do código
