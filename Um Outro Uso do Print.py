48809d4b82cc49cea1c2288b91caee8ec6a616da
Nathalia <156125029+nath9alves@users.noreply.github.com>
2025-02-25 22:39:54 -0300

    Create Um Outro Uso do Print.py

---
+++ Um Outro Uso do Print.py
@@ -0,0 +1,32 @@
+print('Meu nome é', 'Python', 'Ok?')    # Ele exibe as três strings de forma unida
+
+print('Meu nome é', 'Python', 'Ok?', sep=' $$ ')     # com sep='texto' ao final do código é realizada a separação
+
+#----------------------------------------------------------------------------------------------
+
+print('Meu nome é Python', 'Ok?')
+print('Monty Python', end='   ....!')     # o comando end='texto' insere um texto ao final do código
+
+#----------------------------------------------------------------------------------------------
+
+# @title Programa para receber dois valores e mostrar a soma
+
+soma1 = 5    # Exemplo de declaração de outra variável
+soma2 = 4    # outro exemplo
+
+soma = soma1 + soma2    # exemplo de operação de soma
+
+# print(soma)    # exibição do resultado
+
+print('O resultado de soma1 e soma2 é:', soma)    # Exibição do resultado da variável
+
+print('O resultado de', soma1, 'mais' , soma2, 'é', soma)
+
+# Exibindo o valor de cada variável:
+
+print(f'O resultado de {soma1} e {soma2} é {soma}:')     # print(f {conteúdo}) serve para formatar a exibição da variável para o conteúdo dela
+
+#----------------------------------------------------------------------------------------------
+
+
+
