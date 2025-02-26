67dfeb094651f1cb88918f4ec83044dbf77dc763
Nathalia <156125029+nath9alves@users.noreply.github.com>
2025-02-25 22:38:22 -0300

    Create Exercícios Variáveis.py

---
+++ Exercícios Variáveis.py
@@ -0,0 +1,57 @@
+# @title Exercício 1 - Variáveis (soma) 
+#Crie um programa para receber dois valores e em seguida exiba a soma deles
+
+
+# Programa para receber dois valores e mostrar a soma
+soma1 = 5    # Exemplo de declaração de outra variável
+soma2 = 4    # outro exemplo
+
+soma = soma1 + soma2    # exemplo de operação de soma
+
+# print(soma)    # exibição do resultado
+
+print('O resultado de soma1 e soma2 é:', soma)    # Exibição do resultado da variável
+print('O resultado da soma é:' + soma)     # exibição de erro <--------------
+
+#----------------------------------------------------------------------------------------------
+
+# @title Exercício 2 - Variáveis (diferença)
+#Crie um programa que recebe dois valores e em seguida exiba a diferença deles
+
+menosum = 56
+meosdois = 23.9
+
+um_menos_dois = menosum - meosdois
+
+print('O resultado da subtração é:', um_menos_dois)
+
+#----------------------------------------------------------------------------------------------
+
+# @title Exercício 3 - Variáveis (divisão)
+#Crie um programa que recebe dois valores e em seguida exibe o quociente deles
+
+divide1 = 100
+divide2 = 20
+
+divisao = divide1 / divide2
+
+print('O resultado da divisão é:', divisao)
+
+#----------------------------------------------------------------------------------------------
+
+# @title Exercício 4 - Variáveis (multiplicação)
+#Crie um programa que recebe dois valores e em seguida exibe o produto deles
+
+vezes_1 = 890
+vezes_2 = 3.98
+
+produto = vezes_1 * vezes_2
+
+print('O resultado da multiplicação é:', produto)
+
+
+
+
+
+
+
