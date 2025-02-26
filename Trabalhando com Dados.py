52241ba043ce0bf7478613a6f1d9ea1837bc54c1
Nathalia <156125029+nath9alves@users.noreply.github.com>
2025-02-25 22:27:28 -0300

    Update and rename Exercício Variáveis.py to Trabalhando com Dados.py

---
+++ Trabalhando com Dados.py
@@ -0,0 +1,7 @@
+print(123)    # o primeiro número --> não é necessário aspas para exibir números
+
+print(12375.08)    # um número fracionário
+print(1_000_000)    # outro número fracionário
+
+print(12.376,08)    # outra forma de declarar um número (certo ou errado?) --> errado, pois não é permitida a vírgula em números decimais
+
