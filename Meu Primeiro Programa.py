b7806599879e4b8ddd64e6ae5f053e484d6a5347
Nathalia <156125029+nath9alves@users.noreply.github.com>
2025-02-25 22:16:04 -0300

    Create Meu Primeiro Programa.py

---
+++ Meu Primeiro Programa.py
@@ -0,0 +1,80 @@
+#**Curso Programação em Python**
+
+SENAI Suiço-Brasileiro em 01 de Fevereiro de 2025
+
+# Meu Primeiro Programa
+"""
+
+print ('Olá SENAI!')    # meu primeiro código python --> print (imprimir/mostrar)  --> aspas simples '' para texto
+
+"""# Meu Primeiro Erro"""
+
+Print('Oi SENAI de novo!')     # meu primeiro erro --> print escrito com P maiúsculo --> as letras maiúsculas e minúsculas são diferenciadas pelo python
+
+print('Oi SENAI Mais uma Vez'    # sintaxe incompleta pois falta um fecha parenteses no final da menagem à ser exibida
+
+print('E agora José?)    # sintaxe indeterminada pois falta uma aspas para determinar que se trata de um texto
+
+"""# Trabalhando com Dados"""
+
+print(123)    # o primeiro número --> não é necessário aspas para exibir números
+
+print(12375.08)    # um número fracionário
+print(1_000_000)    # outro número fracionário
+
+print(12.376,08)    # outra forma de declarar um número (certo ou errado?) --> errado, pois não é permitida a vírgula em números decimais
+
+"""# Trabalhando com Texto"""
+
+print("Eu sou uma String!")    # Exemplo de texto
+
+print('Eu também!')     # os delimitadores (aspas) estão auxiliando o python p/ saber o que eu quero escrever
+
+"""# Trabalhando com Booleanos"""
+
+print(5>2)     # Exemplo de expressão lógica
+
+print(5<2)    # Outro exemplo de expressão lógica
+
+"""# Operações em Python
+
+# Adição
+"""
+
+print(5+35)     # Exemplo de operação de adição
+
+print(5+2.7)    # outro exemplo de adição
+
+"""# Subtração"""
+
+print(345-78)    # Exemplo de operação de subtração
+
+print(1.45-54)    # outro exemplo
+
+"""# Divisão e Divisão Inteira"""
+
+print(100/5)    # Exemplo de divisão simples
+
+print(100//5)    # exemplo de divisão inteira
+
+print(100//5.0)    # exemplo de divisão inteiro e float
+
+"""# Multiplicação"""
+
+print(3*4)    # Exemplo de operação de multiplicação
+
+print(4.5*22)    # outro exemplo com float
+
+"""# Exponenciação"""
+
+print(2**3)    # Exemplo de operação de exponenciação
+
+print(4.8**3)    # outro exemplo com float
+
+"""# Módulo"""
+
+print(10%4)    # Exemplo de operação módulo
+
+print(10%2)    # outro exemplo
+
+print(12%4.5)    # exemplo de divisão com float
