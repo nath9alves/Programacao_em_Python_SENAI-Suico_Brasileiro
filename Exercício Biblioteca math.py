9826ef31fcf15f5ea5cf80f5779c4937b04a7860
Nathalia <156125029+nath9alves@users.noreply.github.com>
2025-02-25 22:21:36 -0300

    Create Exercício Biblioteca math.py

---
+++ Exercício Biblioteca math.py
@@ -0,0 +1,16 @@
+
+"""# Exercício 6 - Biblioteca math
+
+Crie um programa que pede um número e calcula a raíz quadrada dele usando a biblioteca math.
+"""
+
+# Exemplo de programa que usa uma biblioteca externa do python
+# Usa a biblioteca math
+
+import math    # Importando a biblioteca math (biblioteca matemática)
+
+numero = int(input('Digite um número'))
+
+raizquadrada = math.sqrt(numero)    #math.sqrt é biblioteca matemática.Raiz Quadrada
+
+print(f'A raíz quadrada de {numero} é {raizquadrada}')
