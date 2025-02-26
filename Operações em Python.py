44ed75eb04ed1fd5480b6935cf5330938ec1c406
Nathalia <156125029+nath9alves@users.noreply.github.com>
2025-02-25 22:33:21 -0300

    Create Operações em Python.py

---
+++ Operações em Python.py
@@ -0,0 +1,31 @@
+// -> Adição
+
+print(5+35)     # Exemplo de operação de adição
+print(5+2.7)    # outro exemplo de adição
+
+// -> Subtração
+
+print(345-78)    # Exemplo de operação de subtração
+print(1.45-54)    # outro exemplo
+
+// -> Divisão e Divisão Inteira
+
+print(100/5)    # Exemplo de divisão simples
+print(100//5)    # exemplo de divisão inteira
+print(100//5.0)    # exemplo de divisão inteiro e float
+
+// -> Multiplicação
+
+print(3*4)    # Exemplo de operação de multiplicação
+print(4.5*22)    # outro exemplo com float
+
+// -> Exponenciação
+
+print(2**3)    # Exemplo de operação de exponenciação
+print(4.8**3)    # outro exemplo com float
+
+// -> Módulo
+
+print(10%4)    # Exemplo de operação módulo
+print(10%2)    # outro exemplo
+print(12%4.5)    # exemplo de divisão com float
