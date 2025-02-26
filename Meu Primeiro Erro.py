9ba6ff05a9e7d3c184742c780f32c7ec3abdc8b0
Nathalia <156125029+nath9alves@users.noreply.github.com>
2025-02-25 22:26:28 -0300

    Update and rename Exercício Biblioteca math.py to Meu Primeiro Erro.py

---
+++ Meu Primeiro Erro.py
@@ -0,0 +1,5 @@
+Print('Oi SENAI de novo!')     # meu primeiro erro --> print escrito com P maiúsculo --> as letras maiúsculas e minúsculas são diferenciadas pelo python
+
+print('Oi SENAI Mais uma Vez'    # sintaxe incompleta pois falta um fecha parenteses no final da menagem à ser exibida
+
+print('E agora José?)    # sintaxe indeterminada pois falta uma aspas para determinar que se trata de um texto
